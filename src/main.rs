9d3164bc30ab45c6351e0a758d6b06b2c28e5bef
danmartinvela <danmartinvela@gmail.com>
2024-06-21 22:42:31 +0200

    some changes for simplifying the whole process

--- src/main.rs
+++ src/main.rs
@@ -9,67 +9,35 @@ use crate::errors::ConversionError;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    // let args = &args[1..];
-
-    // let mut from: &str;
-    // let mut to: &str;
-    // let mut value: &str;
-
-    // for arg in args {
-    //     if arg == "-in" || arg == "-out" {
-    //         set_env(arg);
-    //         break;
-    //     } else {
-    //     }
-    // }
-
     // // bdh -in b
     // // bdh -out b
     // // bdh -in b -out h
-    // if args.len() != 3 {
-    //     eprintln!("Example: bdh -bd 1111");
-    //     process::exit(1);
-    // }
+    if args.len() != 3 {
+        eprintln!("Example: bdh -b 3FF");
+        process::exit(1);
+    }
 
-    let from = &args[1][0..2];
-    let to = &args[1][2..];
+    let from = &args[1];
     let value = &args[2];
 
-    // match from {
-    //     "-in" => set_in(),
-    //     "-out" => set_out(),
-    // }
-
-    for c in to.chars() {
-        match convert(from, c, value) {
-            Ok(r) => println!("{}", r),
-            Err(e) => eprintln!("Error: {}", e),
-        }
+    match convert(from, value) {
+        Ok(r) => print(r),
+        Err(e) => eprintln!("Error: {}", e),
     }
 }
 
-fn convert(from: &str, to: char, value: &str) -> Result<String, ConversionError> {
-    let decimal_value = match from {
+fn convert(from: &str, value: &str) -> Result<i64, ConversionError> {
+    let decimal = match from {
         "-b" => binary::to_decimal(value)?,
         "-d" => decimal::parse(value)?,
         "-h" => hexadecimal::to_decimal(value)?,
         _ => return Err(ConversionError::InvalidFormat),
     };
-
-    let result = match to {
-        'b' => binary::from_decimal(decimal_value),
-        'd' => decimal::from_decimal(decimal_value),
-        'h' => hexadecimal::from_decimal(decimal_value),
-        _ => return Err(ConversionError::InvalidFormat),
-    };
-
-    Ok(result)
+    Ok(decimal)
 }
 
-// fn set_env(value: &str) {
-//     match value {
-//         "-in" => env::set_var("BDH_IN", value),
-//         "-out" => env::set_var("BDH_OUT", value),
-//         _ => unreachable!(),
-//     }
-// }
+fn print(decimal: i64) {
+    println!("{}", binary::from_decimal(decimal));
+    println!("{}", decimal::from_decimal(decimal));
+    println!("{}", hexadecimal::from_decimal(decimal));
+}
